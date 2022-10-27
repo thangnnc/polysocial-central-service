f283640a219299765962756de19a065a6f1296fb
Duy <hoangduy2812002@gmail.com>
2022-10-27 21:58:55 +0700

    demo-central
    demo-central

--- src/main/java/com/polysocial/consts/CentralAPI.java
+++ src/main/java/com/polysocial/consts/CentralAPI.java
@@ -4,6 +4,8 @@ public class CentralAPI {
 
     public static final String GET_DEMO = "/api/demo-detail";
     public static final String GET_MESSAGE = "/api/message-detail";
-    public static final String GET_POST = "/api/post-detail";
+//    public static final String GET_POST = "/api/post-detail";
     public static final String GET_EXERCISE = "/api/exercise-detail";
+    
+    public static final String GET_ALL_POST = "/api/posts";
 }
--- src/main/java/com/polysocial/rest/controller/post/PostController.java
+++ src/main/java/com/polysocial/rest/controller/post/PostController.java
@@ -1,10 +1,16 @@
 package com.polysocial.rest.controller.post;
 
 import com.polysocial.consts.CentralAPI;
-import com.polysocial.dto.PostDTO;
+import com.polysocial.dto.ListPostDTO;
 import com.polysocial.service.post.PostService;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,14 +18,11 @@ public class PostController {
 
     @Autowired
     private PostService postService;
-
-    @GetMapping(CentralAPI.GET_POST)
-    public PostDTO getPost(){
-        PostDTO response = postService.getPost();
-        return response;
-    }
     
-    public String pushTest() {
-    	return null;
+    @GetMapping(CentralAPI.GET_ALL_POST)
+    public ResponseEntity getAllPost(@RequestParam("page") Optional<Integer> page,
+            @RequestParam("limit") Optional<Integer> limit) {
+        ListPostDTO response = postService.getAllPosts(page.orElse(0), limit.orElse(10));
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
