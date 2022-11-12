2fd8d9af9117079a4612ab064502910639777d13
Duy <hoangduy2812002@gmail.com>
2022-11-12 12:36:05 +0700

    post
    lan1

--- src/main/java/com/polysocial/dto/PostDTO.java
+++ src/main/java/com/polysocial/dto/PostDTO.java
@@ -2,30 +2,34 @@ package com.polysocial.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.polysocial.entity.Comments;
+import com.polysocial.entity.Groups;
+import com.polysocial.entity.Likes;
+import com.polysocial.entity.PostFile;
+import com.polysocial.entity.Users;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PostDTO implements Serializable {
+    
+	private String content;
 
-	 private Long postId;
+    private Long createdBy;
 
-	    private String content;
-
-	    private Date createdDate;
-
-	    private Long createdBy;
-
-	    private Long groupId;
-	    
-	    private Long countLike;
-	    
-	    private Long countComment;
-	    
-		private List<CommentDTO> listComment;
+    private Long groupId;
 }
+
