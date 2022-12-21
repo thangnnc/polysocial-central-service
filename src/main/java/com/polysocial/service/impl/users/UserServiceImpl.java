c43e348c8cbb6760b36d9297a58c25403db13f5c
Phan Nguyen Dang Truong <96562712+phannguyendangtruong@users.noreply.github.com>
2022-12-21 16:15:24 +0700

    update

--- src/main/java/com/polysocial/service/impl/users/UserServiceImpl.java
+++ src/main/java/com/polysocial/service/impl/users/UserServiceImpl.java
@@ -304,23 +304,22 @@ public class UserServiceImpl implements UserService {
                 userRepo.findById(userInviteId).get().getFullName(), userRepo.findById(userInviteId).get().getAvatar(),
                 userRepo.findById(userConfirmId).get().getAvatar());
         String nameGroup = friendDetailDTO.getFullNameUserConfirm() + "," + friendDetailDTO.getFullNameUserInvite();
-        Groups groupCheck = groupRepo.findGroupByName(nameGroup);
-        if(groupCheck == null){
-            return friendDetailDTO;
-        }
         friendDetailDTO.setStatus(true);
         friendDetailDTO.setEmailInvite(userRepo.findById(userInviteId).get().getEmail());
-        groupCheck = new Groups(nameGroup, null,
+        Groups groupCheck = new Groups(nameGroup, null,
                 "Friend with chat rooms", "Friends chat");
         groupCheck.setAvatar(friendDetailDTO.getAvatarUserConfirm() + "," + friendDetailDTO.getAvatarUserInvite());
         Groups groupCreated = groupRepo.save(groupCheck);
-        fr.setGroup(groupCreated);
+        // fr.setGroup(groupCreated);
+        // friendRepo.save(fr);
         RoomChats roomChat = new RoomChats(groupCreated);
         roomChat.setLastMessage("Hai bạn đã trở thành bạn bè của nhau");
         // encodedString
         String encodedStringRoom = Base64.getEncoder().encodeToString(roomChat.getLastMessage().getBytes());
         roomChat.setLastMessage(encodedStringRoom);
+        System.out.println("123");
         Long roomChatId = roomChatRepo.save(roomChat).getRoomId();
+        System.out.println("213");
         friendDetailDTO.setRoomId(roomChatId);
         friendDetailDTO.setGroupId(groupCreated.getGroupId());
         Contacts contact = new Contacts(userConfirmId, roomChatId);
