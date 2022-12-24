1ddc1d9c4a407217fc9d6fc964e3ab8e3d7456a4
Phan Nguyen Dang Truong <96562712+phannguyendangtruong@users.noreply.github.com>
2022-12-24 16:47:52 +0700

    update

--- src/main/java/com/polysocial/dto/MemberGroupDTO.java
+++ src/main/java/com/polysocial/dto/MemberGroupDTO.java
@@ -1,6 +1,7 @@
 package com.polysocial.dto;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class MemberGroupDTO implements Serializable{
+public class MemberGroupDTO implements Serializable {
     private Long groupId;
     private Long userId;
     private String groupName;
@@ -17,9 +18,11 @@ public class MemberGroupDTO implements Serializable{
     private String avatarGroup;
     private Long roomId;
     private List<ContactDTO> listContact;
+    private String description;
+    private LocalDateTime createdDate;
 
-
-    public MemberGroupDTO(Long groupId, Long userId,  String groupName, Long totalMember, String avatarGroup, Long roomId ) {
+    public MemberGroupDTO(Long groupId, Long userId, String groupName, Long totalMember, String avatarGroup,
+            Long roomId) {
         this.groupId = groupId;
         this.userId = userId;
         this.groupName = groupName;
