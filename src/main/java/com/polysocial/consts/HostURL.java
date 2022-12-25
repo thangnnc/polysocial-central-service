59c3c35dfb538efb1b12cf27fec783e96950a496
thangnnc <77538291+thangnncps14579@users.noreply.github.com>
2022-12-25 17:03:57 +0700

    congif to deploy

--- src/main/java/com/polysocial/consts/HostURL.java
+++ src/main/java/com/polysocial/consts/HostURL.java
@@ -3,9 +3,9 @@ package com.polysocial.consts;
 public class HostURL {
 
     public static final String DEMO_HOST = "http://localhost:8110/demo";
-    public static final String POST_HOST = "http://localhost:8111/post";
-    public static final String MESSAGE_HOST = "http://localhost:8112/message";
-    public static final String GROUP_HOST = "http://localhost:8113/group";
+    public static final String POST_HOST = "https://polysocial-post-backend.up.railway.app/post";
+    public static final String MESSAGE_HOST = "https://polysocial-message-backend.up.railway.app/message";
+    public static final String GROUP_HOST = "https://polysocial-group-service.up.railway.app/group";
     public static final String FILE_UPLOAD_HOST = "http://localhost:8114/file-upload";
     public static final String EX_QUIZ_HOST = "http://localhost:8115/ex-quiz";
 
